e4b458767d3151a047c50441b35a9fdc9e9d16ff
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2023-09-14 13:24:53 +0300

    Update cache.js

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,19 @@
 class Cache{
+    cache = new Map();
+    hits = new Map();
+    
+    constructor(messagesBeforeTrash)
+    {
+        this.messagesBeforeTrash = messagesBeforeTrash;
+    }
+
+    showCache()
+    {
+        if(this.messagesBeforeTrash == 0)
+        {
+            return null;
+        }
+    }
 
 }
 export {Cache}
