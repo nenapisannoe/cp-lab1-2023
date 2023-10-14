9b8338286b1ff89bee391bbe854be87b76c68bc4
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2023-10-14 14:56:21 +0300

    its been 11 years

--- src/cache.js
+++ src/cache.js
@@ -1,18 +1,37 @@
 class Cache{
-    cache = new Map();
-    hits = new Map();
-    
-    constructor(messagesBeforeTrash)
+
+    constructor()
     {
-        this.messagesBeforeTrash = messagesBeforeTrash;
+        this.data = new Map();
+        this.log = [];
     }
 
-    showCache()
+    set(key, value, _queries = 1)
     {
-        if(this.messagesBeforeTrash == 0)
+        this.data.set(key, {value: value, queries: _queries})
+    }
+    
+    get(key)
+    {
+        const res = this.data.get(key);
+
+        if(res == undefined)
+            return null;
+        if(res.queries == 0)
         {
+            this.data.delete(key);
             return null;
         }
+
+        res.queries--;
+        this.log.push({key: key, value: res.value, queries: res.queries})
+        return res.value;
+
+    }
+
+    logs()
+    {
+        return this.log;
     }
 
 }
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,62 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+describe('get/set working properly', () => {     
+    it('returns null if there is no value by given key', () => {
+        const testKey = "i am key";
+        let testCache = new Cache();
+    
+        let gotValue = testCache.get(testKey);
+    
+        expect(gotValue).toBe(null);
+    });
+        
+    it('set working properly', () => {
+        const testKey = "i am key";
+        const testValue = "i am value";
+        let testCache = new Cache();
+    
+        testCache.set(testKey, testValue);
+    
+        let gotValue = testCache.get(testKey);
+        expect(gotValue).toBe(testValue);
+    });
+
+});
+
+describe('cache expiring properly', () => {     
+    it('queries amount being 1 by default', () =>{
+        const testKey = "i am key";
+        const testValue = "i am value";
+
+        let testCache = new Cache();
+        testCache.set(testKey, testValue);
+
+        testCache.get(testKey);
+        expect(testCache.get(testKey)).toBe(null);
+    })
+
+    it('cache expires after given amount of queries', () => {
+        const testKey = "i am key";
+        const testValue = "i am value";
+        const queries = 2;
+
+        let testCache = new Cache();
+    
+        testCache.set(testKey, testValue, queries);
+
+        testCache.get(testKey);
+        testCache.get(testKey);
+    
+        expect(testCache.get(testKey)).toBe(null);
+    });
+});
+
+describe('cache log working properly', () =>{
+    it('returns empty array if no queries', () =>
+    {
+        let testCache = new Cache();
+
+        const testLog = testCache.logs();
+        expect(testLog).toEqual([]);
+    });
 });
